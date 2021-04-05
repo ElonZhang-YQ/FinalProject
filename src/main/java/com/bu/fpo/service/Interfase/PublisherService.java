318f5fdeec95ef7e548ecc8e189cac04638bd66f
Elon Zhang <clearlovezyq@gmail.com>
2021-04-04 21:25:29 -0400

    Elon Zhang: Service Implement and function solution

--- src/main/java/com/bu/fpo/service/Interfase/PublisherService.java
+++ src/main/java/com/bu/fpo/service/Interfase/PublisherService.java
@@ -1,10 +1,24 @@
 package com.bu.fpo.service.Interfase;
 
+import com.bu.fpo.obj.Publisher;
+
+import java.util.List;
+
 /**
  * This class created on 4/2/2021
  *
  * @author Elon.Zhang
  */
 public interface PublisherService {
+
+    List<Publisher> findAllPublisher();
+    
+    Publisher findPublisherById(String publisherId);
+    
+    boolean createNewPublisher(Publisher publisher);
+    
+    boolean changePublisherProfile(String publisherId, Publisher publisher);
+    
+    boolean removePublisher(Publisher publisher);
 
 }
--- src/main/java/com/bu/fpo/service/UserServiceImp.java
+++ src/main/java/com/bu/fpo/service/UserServiceImp.java
@@ -1,10 +1,44 @@
 package com.bu.fpo.service;
 
+import com.bu.fpo.obj.NormalUser;
+import com.bu.fpo.service.Interfase.UserService;
+
+import java.util.List;
+
 /**
  * This class created on 4/2/2021
  *
  * @author Elon.Zhang
  */
-public class UserServiceImp {
-
+public class UserServiceImp implements UserService {
+    
+    @Override
+    public List<NormalUser> findAllUsers() {
+        
+        return null;
+    }
+    
+    @Override
+    public NormalUser findUserById(String userId) {
+        
+        return null;
+    }
+    
+    @Override
+    public boolean changeUserProfile(String userId, NormalUser currentUser) {
+        
+        return false;
+    }
+    
+    @Override
+    public boolean SignUpUser(NormalUser newUser) {
+        
+        return false;
+    }
+    
+    @Override
+    public boolean removeUser(NormalUser deleteUser) {
+        
+        return false;
+    }
 }
