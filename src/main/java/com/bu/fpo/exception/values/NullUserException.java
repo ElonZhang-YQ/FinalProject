d7e74480be414825d5908a13a04f3de22c0566a5
Elon Zhang <clearlovezyq@gmail.com>
2021-04-09 21:55:31 -0400

    Elon Zhang: Exception extend change

--- src/main/java/com/bu/fpo/exception/values/NullUserException.java
+++ src/main/java/com/bu/fpo/exception/values/NullUserException.java
@@ -1,10 +1,12 @@
 package com.bu.fpo.exception.values;
 
+import com.bu.fpo.exception.interfase.FPOException;
+
 /**
  * This class created on 3/30/2021
  *
  * @author Elon.Zhang
  */
-public class NullUserException {
+public class NullUserException extends FPOException {
 
 }
--- src/main/java/com/bu/fpo/exception/values/SameValueException.java
+++ src/main/java/com/bu/fpo/exception/values/SameValueException.java
@@ -1,10 +1,14 @@
 package com.bu.fpo.exception.values;
 
+import com.bu.fpo.exception.interfase.FPOException;
+
 /**
  * This class created on 2021-04-05
  *
  * @author Each.Zhang
  */
-public class SameValueException extends NullKeyException {
+public class SameValueException extends FPOException {
+    
+    private 
     
 }
